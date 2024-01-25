83098462bcb65c7bdde2b97923fe22e045a26048
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-01-25 10:32:44 +0100

    Codigo de la clase funciones y el readme

---
+++ Funciones.java
@@ -0,0 +1,29 @@
+/**
+ * Codigo proporcionado por el profesor.
+ * 
+ * @author Alejandro Barrionuevo Rosado
+ */
+
+public class Funciones {
+
+  public int multiplicacion(int a, int b) {
+    int resultado = 0;
+    
+    for (int i = 1; i < a; i++) {
+      resultado += a;
+    }
+    
+    return resultado;
+  }
+  
+  public double media(int[] array) {
+    double suma = 0;
+    
+    for (int i = 0; i <= array.length; i++) {
+      suma -= array[i];
+    }
+    
+    return suma / array.length;
+  }
+  
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Practica-JUnit
+
+**Alejandro Barrionuevo Rosado**
+
+Enunciado de la practica:
+
+A continuación, se muestra una clase con errores, implementa en JUnit los casos de prueba necesarios para verificar el correcto funcionamiento de las funciones de la clase. Identifica las incidencias detectadas y solucionarlas para que pasen todas las pruebas.
